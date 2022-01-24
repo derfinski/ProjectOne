586de72d7e02ef39ec7f7589934f5c06d25ffa44
Fin Eckhoff <fin.eckhoff@unibw.de>
2022-01-24 10:41:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projectone/IPv4Validator.java
@@ -0,0 +1,21 @@
+package com.example.projectone;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IPv4Validator {
+
+    private static final String IPV4_PATTERN =
+            "^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\." +
+                    "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\." +
+                    "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\." +
+                    "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$";
+
+    private static final Pattern pattern = Pattern.compile(IPV4_PATTERN);
+
+    public static boolean isValid(final String ip) {
+        Matcher matcher = pattern.matcher(ip);
+        return matcher.matches();
+    }
+
+}
